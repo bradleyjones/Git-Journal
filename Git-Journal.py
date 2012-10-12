6cc488bd797d6507b8e9c767e97f15c323ba2df9
Bradley Jones <jones.bradley@me.com>
2012-10-12 00:24:33 -0700

    Sets markdown based on -m argument

--- Git-Journal.py
+++ Git-Journal.py
@@ -7,19 +7,20 @@ from git import *
 def main(argv):
     gitFolder = ''
     gitEmail = ''
+    markdown = False
     try:
-        opts, args = getopt.getopt(argv,"m:hf:e:",["","gfold=","gemail="])
+        opts, args = getopt.getopt(argv,"mhf:e:",["gfold=","gemail="])
     except getopt.GetoptError:
         print 'Git-Journal.py -f <gitFolder> -e <gitEmail>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print 'Git-Journal.py -f <gitFolder> -e <gitEmail>'
-        elif opt in ("-m", "--"):
-            print 'Using Markdown'
+        elif opt == '-m':
+            markdown = True
         elif opt in ("-f", "--gfold"):
             gitFolder = arg
-            print 'Git Folder is "',gitFolder,'"'
+            #print 'Git Folder is "',gitFolder,'"'
         elif opt in ("-e", "--gemail"):
             gitEmail = arg
             #print 'Git Email is "',gitEmail,'"'
@@ -31,7 +32,8 @@ def main(argv):
         print "The Git folder you entered is not a valid Git repository"
         sys.exit(2)
 
-    print "**Today in Git!** \n"
+    if markdown:
+        print "**Today in Git!** \n"
     
     for x in repo.iter_commits('master'):
         if x.author.email == gitEmail:
@@ -39,13 +41,17 @@ def main(argv):
             today = strftime("%d %b %Y", localtime()) 
             if commitDate != today:
                 break
-            #make commit message italic
+
             commitMsg = ""
             for line in x.message.split('\n'):
                 if line != "": #Remove lines that are empty
-                    commitMsg += "*" + line + "*" + "\n"
+                    if markdown:
+                        commitMsg += "*" + line + "*" + "\n"
+                    else:
+                        commitMsg += line + "\n"
             print commitMsg
-            print "***"
+            if markdown:
+                print "***"
 
 if __name__ == "__main__":
    main(sys.argv[1:])
