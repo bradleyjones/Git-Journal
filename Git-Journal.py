b01174ccec676d884d2ddf603132078db53b3dc2
Bradley Jones <jones.bradley@me.com>
2012-10-11 21:15:45 -0700

    Gets all the messages from today for a specific user - determines the user from an email address passed as an argument. --- Detects whether the commit message ended with a line break if not then it adds a line break to the message

--- Git-Journal.py
+++ Git-Journal.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
 
 import sys, getopt
+from time import localtime, strftime
 from git import *
 
 def main(argv):
     gitFolder = ''
+    gitEmail = ''
     try:
-        opts, args = getopt.getopt(argv,"hl:o:",["gfold="])
+        opts, args = getopt.getopt(argv,"hf:e:",["gfold=","gemail"])
     except getopt.GetoptError:
-        print 'test.py -l <gitFolder>'
+        print 'Git-Journal.py -f <gitFolder> -e <gitEmail>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -l <gitFolder>'
+            print 'Git-Journal.py -f <gitFolder> -e <gitEmail>'
             sys.exit()
-        elif opt in ("-l", "--gfold"):
+        elif opt in ("-f", "--gfold"):
             gitFolder = arg
-            print 'Git Folder is "',gitFolder,'"'
+            #print 'Git Folder is "',gitFolder,'"'
+        elif opt in ("-e", "--gemail"):
+            gitEmail = arg
+            #print 'Git Email is "',gitEmail,'"'
     
-    if len(sys.argv) == 1:
-        print 'Please pass the folder of the Git repo'
-        print 'test.py -l <gitFolder>'
-        sys.exit(2)
-    
+    #Set repository
     repo = Repo(gitFolder)
-    print repo
 
     for x in repo.iter_commits('master'):
-        print x.message
+        if x.author.email == gitEmail:
+            commitDate = strftime("%d %b %Y", localtime(x.committed_date)) 
+            today = strftime("%d %b %Y", localtime()) 
+            if commitDate != today:
+                break
+            commitMsg = x.message
+            if commitMsg.endswith("\n"):
+                print commitMsg
+            else:
+                print commitMsg + "\n"
 
 if __name__ == "__main__":
    main(sys.argv[1:])
